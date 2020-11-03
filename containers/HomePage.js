f88f4ea24ccfbf8395d40cd7e103335910a5d3b1
maroqijalil <jalilpro31@gmail.com>
2020-11-03 13:54:37 +0700

    init main structure

---
+++ containers/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {View, Text, Image, Button} from "react-native"
+
+class HomePage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
---
+++ containers/ListPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {View, Text, Image, Button} from "react-native"
+
+class ListPage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
