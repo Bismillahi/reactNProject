f59d39b130daad4a8920f70e4f3d243ce8a58537
maroqijalil <jalilpro31@gmail.com>
2020-11-10 02:19:51 +0700

    init list

--- components/PopularList.js
+++ components/PopularList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet,
+    Image, FlatList, TouchableOpacity } from "react-native";
 
 const API_DETAIL_URL = "https://api.themoviedb.org/3/movie/"
 const API_KEY = "c696ae5550ca0ba0e92a7be8d9a60acf"
@@ -8,8 +9,13 @@ const PopularList = props => {
     const { dataSource } = props
 
     return(
-      <View>
-      </View>
+      <FlatList
+        data={dataSource}
+        horizontal={true}
+        renderItem={({item, index, separators}) => (
+          <TouchableOpacity>
+          </TouchableOpacity>
+        )}/>
     );
 }
 
--- components/UpcomingList.js
+++ components/UpcomingList.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet,
+    Image, FlatList, TouchableOpacity,
+    ImageBackground } from "react-native";
 
 const API_DETAIL_URL = "https://api.themoviedb.org/3/movie/"
 const API_KEY = "c696ae5550ca0ba0e92a7be8d9a60acf"
@@ -8,9 +10,26 @@ const UpcomingList = props => {
     const { dataSource } = props
 
     return(
-        <View>
-        </View>
+        <FlatList
+            data={dataSource}
+            horizontal={true}
+            renderItem={({item, index, separators}) => (
+                <TouchableOpacity>
+                    <ImageBackground
+                        style={styles.imageBG}
+                        source={{ uri: dataSource.backdrop}}>
+
+                    </ImageBackground>
+                </TouchableOpacity>
+            )}/>
     );
 }
+
+const styles = StyleSheet.create({
+    imageBG: {
+        resizeMode: "cover",
+        justifyContent: "center"
+    }
+})
 
 export default UpcomingList;
